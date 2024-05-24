38d0429d388e15220efc11c4b72c49da25ea1934
TudorCalinCS <panzaru.tudor@gmail.com>
2024-05-24 17:59:45 +0300

    commit

---
+++ test.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+public class test {
+    public static void main(String[] args) {
+        System.out.println("null");
+
+        Queue<String> list= new ArrayDeque<>() ;
+        for(int i=0;i<list.size();i++){ 
+                
+        }
+
+    }
+}
